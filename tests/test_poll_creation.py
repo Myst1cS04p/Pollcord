1a1c01a6c0af29b6f9cba4e17211e4d0a399596b
Myst1cS04p <myst1cs04p@gmail.com>
2026-02-25 11:06:23 +0300

    Remove unused imports

--- tests/test_poll_creation.py
+++ tests/test_poll_creation.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from aioresponses import aioresponses
 from Pollcord import PollClient, Poll
 from Pollcord.error import PollCreationError
